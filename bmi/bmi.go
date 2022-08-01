46017883c8d201a61764f8cb614d3ede5ea6bddd
HowieChy <chenhaoyun@rthinkai.com>
2022-08-01 21:15:19 +0800

    用函数优化

--- bmi/bmi.go
+++ bmi/bmi.go
@@ -1,34 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
-
 	"github.com/HowieChy/Go/bmi/info"
 )
 
 func main() {
-	// Output information
-	fmt.Println(info.MainTitle)
-	fmt.Println(info.Separator)
-	// Prompt for user input (weight + height)
-	fmt.Print(info.WeightPrompt)
-	weightInput, _ := reader.ReadString('\n')
 
-	fmt.Print(info.HeightPrompt)
-	heightInput, _ := reader.ReadString('\n')
+	info.PrintWelcome()
 
-	// Save that user input in variables
-	weightInput = strings.Replace(weightInput, "\n", "", -1)
-	heightInput = strings.Replace(heightInput, "\n", "", -1)
+	weight, height := getUserMetrics()
 
-	weight, _ := strconv.ParseFloat(weightInput, 64)
-	height, _ := strconv.ParseFloat(heightInput, 64)
+	bmi := calculateBMI(weight, height)
 
-	// Calculate the BMI (weight / (height * height))
-	bmi := (weight / (height * height)) * 100 * 100
+	printBMI(bmi)
+}
 
-	// Output the calculated BMI
-	fmt.Printf("Your BMI: %.2f\n", bmi)
+func calculateBMI(weight float64, height float64) float64 {
+	return weight / (height * height) * 100 * 100
 }
--- bmi/input.go
+++ bmi/input.go
@@ -2,7 +2,33 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/HowieChy/Go/bmi/info"
 )
 
+//从标准输入中即命令行中获取
 var reader = bufio.NewReader(os.Stdin)
+
+func getUserMetrics() (weight float64, height float64) {
+	weight = getUserInput(info.WeightPrompt)
+	height = getUserInput(info.HeightPrompt)
+
+	return
+}
+
+func getUserInput(promptText string) (value float64) {
+	fmt.Print(promptText)
+
+	// \n表示换行的时候去读取
+	userInput, _ := reader.ReadString('\n')
+	//去掉换行空格
+	userInput = strings.Replace(userInput, "\n", "", -1)
+	//字符串转数字
+	value, _ = strconv.ParseFloat(userInput, 64)
+
+	return
+}
