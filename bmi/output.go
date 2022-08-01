3b59d3f04e2aa5310103041745060b57d8e8aff8
HowieChy <chenhaoyun@rthinkai.com>
2022-08-01 21:45:49 +0800

    安装第三方module

--- bmi/output.go
+++ bmi/output.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"strconv"
+
+	"github.com/common-nighthawk/go-figure"
+)
 
 func printBMI(bmi float64) {
-	fmt.Printf("Your BMI: %.2f", bmi)
+	// fmt.Printf("Your BMI: %.2f", bmi)
+	myFigure := figure.NewFigure(strconv.FormatFloat(bmi, 'f', 2, 32), "", true)
+	myFigure.Print()
 }
